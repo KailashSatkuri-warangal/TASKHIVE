71b27346667b62b4ac0d060e210ede8c714aa984
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2025-09-20 20:29:31 +0530

    update mongo atlas online

--- frontend/taskhive/src/api.js
+++ frontend/taskhive/src/api.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-	baseURL: 'http://localhost:5000/api',
+	baseURL: 'https://taskhive-backend.onrender.com/api',
 	headers: { 'Content-Type': 'application/json' }
 });
