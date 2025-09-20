e8d40f42e6eed56ca461b396cf44bd02e6051564
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2025-09-20 20:43:52 +0530

    render issues

--- frontend/taskhive/src/api.js
+++ frontend/taskhive/src/api.js
@@ -1,4 +1,10 @@
-// src/api.js
+// // src/api.js
+// import axios from 'axios';
+
+// export const api = axios.create({
+// 	baseURL: 'http://localhost:5000/api',
+// 	headers: { 'Content-Type': 'application/json' }
+// });
 import axios from 'axios';
 
 export const api = axios.create({
