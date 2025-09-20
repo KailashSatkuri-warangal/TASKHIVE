94cf1fe21cca6f4507a72c8a6df588e191e0d2b3
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2025-09-20 19:56:46 +0530

    update the render

--- frontend/taskhive/src/components/Footer.jsx
+++ frontend/taskhive/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
 	return (
 		<footer className="footer">
-			<p>© Kailash 2025</p>
+			<p>© Kailash | Akhilanandateja | Sai Sruthi | Arun Kumar @2025</p>
 		</footer>
 	);
 }
